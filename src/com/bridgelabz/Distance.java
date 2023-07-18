24cbeb8f57a26e6d1d2febe746120dc65a7da0ac
kirtimadam <104613258+kirtimadam@users.noreply.github.com>
2023-07-18 20:28:03 +0530

    added : ditance small change

--- src/com/bridgelabz/Distance.java
+++ src/com/bridgelabz/Distance.java
@@ -7,22 +7,20 @@ public class Distance {
         //Write a program Distance.java that takes two integer command-line arguments x and y
         // and prints the Euclidean distance from the point (x, y) to the origin (0, 0).
         // The formulae to calculate distance = sqrt(x*x + y*y). Use Math.power function
-
+        double x1 = 0, y1 = 0 , distance;
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter the x :");
-         double x =sc.nextDouble();
+        System.out.println("enter the x2 :");
+        double x2 = sc.nextDouble();
 
-        System.out.println("enter the y :");
-        double y =sc.nextDouble();
+        System.out.println("enter the y2 :");
+        double y2 = sc.nextDouble();
 
+      //  distance = sqrt(x*x + y*y).
 
-        //distance = sqrt(x*x + y*y).
-//
-        double distance = Math.sqrt(Math.pow(x,x) + Math.pow(y,y));
+        distance = Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
         System.out.println(distance);
 
 
-
-
     }
+//
 }
